b7347ea3145d4a179f8e4b62e6ed9b3c26d425ea
Pragati Gupta <pragatigupta0104@gmail.com>
2024-12-02 11:51:27 +0530

    index

--- table2.js
+++ table2.js
@@ -17,4 +17,31 @@ function mydelete(id){
     method:'DELETE'
 })
 .then(res=>alert("deleted successfully..!!!"))
+}async function myedit(id){
+    let myupdata = await fetch(`http://localhost:3000/students/${id}`)
+    let redata = await myupdata.json()
+    let senddata = `
+    <input type="text" value="${redata.id}" id="id1" readonly><br>
+    <input type="text" value="${redata.name}" id="name1"><br>
+    <input type="text" value="${redata.age}" id="age1"><br>
+    <input type="text" value="${redata.address}" id="address1"><br>
+    <input type="submit" onclick="finalupdate('${redata.id}')">
+    `
+    document.querySelector('#edittable').innerHTML = senddata
+}
+
+function finalupdate(id){
+    let fdata={
+        name:document.querySelector('#name1').value,
+         age:document.querySelector('#age1').value,
+         address:document.querySelector('#address1').value,
+    }
+    fetch(`http://localhost:3000/students/${id}`,{
+        method:'PUT',
+        headers:{
+            'Content-type':'application/json'
+        },
+        body:JSON.stringify(fdata)
+    })
+    .then(r=>alert("updated......!!!!"))
 }
